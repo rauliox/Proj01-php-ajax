0c1511354c164d2221a33be00f72680983662241
miusuario <raulbarrantesu@gmail.com>
2016-02-23 16:20:07 -0500

    nuevo commit 2

---
+++ js/ajax.js
@@ -0,0 +1,9 @@
+       $(document).ready(function(){
+            $('form').click(function(){
+                           
+               alert($('form').html()); 
+                
+            });
+            
+            
+        });
